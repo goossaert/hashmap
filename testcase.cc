775954f53ab847aa20c742fd204e3af97584ce22
Emmanuel Goossaert <emmanuel.goossaert@gmail.com>
2013-09-24 00:42:35 +0200

    Moved test cases to a separate file.

---
+++ testcase.cc
@@ -0,0 +1,294 @@
+#include "testcase.h"
+
+namespace hashmap
+{
+
+int exists_or_mkdir(const char *path) {
+  struct stat sb;
+
+  if (stat(path, &sb) == 0) {
+    if (!S_ISDIR(sb.st_mode)) {
+      return 1;
+    }
+  } else if (mkdir(path, 0777) != 0) {
+    return 1;
+  }
+
+  return 0;
+}
+
+
+void TestCase::InsertEntries(uint32_t num_items, std::set<std::string>& keys) {
+  std::string key;
+  int key_size = 16;
+  char buffer[key_size + 1];
+  buffer[key_size] = '\0';
+  char alpha[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  std::set<std::string>::iterator it_find;
+
+  for (uint32_t j = 0; j < num_items; j++) {
+    bool is_valid = false;
+    while (!is_valid) {
+      for (int k = 0; k < key_size; k++) {
+        buffer[k] = alpha[rand() % 62];
+      }
+      key = buffer;
+      it_find = keys.find(key);
+      if (it_find == keys.end()) {
+        is_valid = true;
+      } else {
+        //fprintf(stdout, "%s\n", key.c_str());
+        //fprintf(stdout, "%d\n", keys.size());
+      }
+    }
+    keys.insert(key);
+    int ret_put = hm_->Put(key, key);
+    //fprintf(stderr, "Put() [%s]\n", key.c_str());
+    if (ret_put != 0) {
+      fprintf(stderr, "Put() error\n");
+    }
+  }
+}
+
+
+void TestCase::RemoveEntries(uint32_t num_items, std::set<std::string>& keys) {
+  for (uint32_t index_del = 0; index_del < num_items; index_del++) {
+    uint64_t r = rand();
+    uint64_t offset = r % keys.size();
+    //printf("delete index %d -- offset %llu -- rand %llu\n", index_del, offset, r);
+    std::set<std::string>::iterator it(keys.begin());
+    std::advance(it, offset);
+    //fprintf(stdout, "str: %s\n", (*it).c_str());
+    //key = buffer;
+    int ret_remove = hm_->Remove(*it);
+    //fprintf(stderr, "Remove() [%s]\n", it->c_str());
+    if (ret_remove != 0) fprintf(stderr, "Error while removing\n");
+    keys.erase(it);
+  }
+}
+
+
+
+
+void BatchTestCase::run() {
+  std::set<std::string> keys;
+  std::string key;
+  int key_size = 16;
+  char buffer[key_size + 1];
+  buffer[key_size] = '\0';
+  char filename[1024];
+  char alpha[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  uint32_t num_items;
+  uint32_t num_items_big = (uint32_t)((double)num_buckets_ * load_factor_max_);
+  uint32_t num_items_small = (uint32_t)((double)num_buckets_ * load_factor_remove_);
+  fprintf(stdout, "num_items %u %u\n", num_items_big, num_items_small);
+
+  std::string testcase = "batch";
+  std::string directory = "results";
+  if (exists_or_mkdir(directory.c_str()) != 0) {
+    fprintf(stderr, "Could not create directory [%s]\n", testcase.c_str());
+    exit(1);
+  }
+
+  char pt_string[1024];
+  sprintf(pt_string, "lfm%.2f-lfr%.2f", load_factor_max_, load_factor_remove_);
+
+  char pt_json[1024];
+  sprintf(pt_json, "{\"load_factor_max\": %.2f, \"load_factor_remove\": %.2f}", load_factor_max_, load_factor_remove_);
+
+
+  std::set<std::string>::iterator it_find;
+  for (int i = 0; i < 10; i++) {
+    num_items = num_items_big;
+    srand(i);
+    keys.clear();
+    hm_->Open();
+    for (int cycle = 0; cycle < 50; cycle++) {
+      fprintf(stderr, "instance %d cycle %d\n", i, cycle);
+      for (uint32_t j = 0; j < num_items; j++) {
+        bool is_valid = false;
+        while (!is_valid) {
+          for (int k = 0; k < key_size; k++) {
+            buffer[k] = alpha[rand() % 62];
+          }
+          key = buffer;
+          it_find = keys.find(key);
+          if (it_find == keys.end()) {
+            is_valid = true;
+          } else {
+            //fprintf(stdout, "%s\n", key.c_str());
+            //fprintf(stdout, "%d\n", keys.size());
+          }
+        }
+        keys.insert(key);
+        int ret_put = hm_->Put(key, key);
+        //fprintf(stderr, "Put() [%s]\n", key.c_str());
+        if (ret_put != 0) {
+          fprintf(stderr, "Put() error\n");
+        }
+      }
+      printf("keys insert %zu\n", keys.size());
+
+      hm_->monitoring_->SetTestcase(testcase);
+      hm_->monitoring_->SetInstance(i);
+      hm_->monitoring_->SetCycle(cycle);
+      hm_->monitoring_->SetParametersTestcaseString(pt_string);
+      hm_->monitoring_->SetParametersTestcaseJson(pt_json);
+
+      std::map<std::string, std::string> metadata;
+      hm_->GetMetadata(metadata);
+
+      sprintf(filename, "%s/%s-%s-%s--%s-density--instance%05d-cycle%04d.json", directory.c_str(), testcase.c_str(), metadata["name"].c_str(), metadata["parameters_hashmap_string"].c_str(), pt_string, i, cycle);
+      hm_->monitoring_->PrintDensity(filename);
+
+      //fprintf(stderr, "PrintDensity() out\n");
+      //sprintf(filename, "%s/%s-%s-num_scanned_blocks-%05d-%04d.json", testcase.c_str(), testcase.c_str(), metadata["name"].c_str(), i, cycle);
+      //hm_->monitoring_->PrintNumScannedBlocks(filename);
+
+      sprintf(filename, "%s/%s-%s-%s--%s-psl--instance%05d-cycle%04d.json", directory.c_str(), testcase.c_str(), metadata["name"].c_str(), metadata["parameters_hashmap_string"].c_str(), pt_string, i, cycle);
+      fprintf(stderr, "filename psl %s\n", filename);
+      hm_->monitoring_->PrintProbingSequenceLengthSearch(filename);
+      
+      for (uint32_t index_del = 0; index_del < num_items_small; index_del++) {
+        uint64_t r = rand();
+        uint64_t offset = r % keys.size();
+        //printf("delete index %d -- offset %llu -- rand %llu\n", index_del, offset, r);
+        std::set<std::string>::iterator it(keys.begin());
+        std::advance(it, offset);
+        //fprintf(stdout, "str: %s\n", (*it).c_str());
+        //key = buffer;
+        int ret_remove = hm_->Remove(*it);
+        //fprintf(stderr, "Remove() [%s]\n", it->c_str());
+        if (ret_remove != 0) fprintf(stderr, "Error while removing\n");
+        keys.erase(it);
+      }
+      printf("keys erase %zu\n", keys.size());
+      num_items = num_items_small;
+    }
+
+    fprintf(stderr, "close\n");
+    hm_->Close();
+    fprintf(stderr, "ok\n");
+  }
+
+  // testcase-algo-metric-runnumber-step.json
+  // batch50-shadow-density-00001-0001.json
+  //hm_->monitoring_->PrintDensity("density.json");
+  //hm_->monitoring_->PrintNumScannedBlocks("num_scanned_blocks.json");
+}
+
+
+
+
+void RippleTestCase::run() {
+  std::set<std::string> keys;
+  std::string key;
+  int key_size = 16;
+  char buffer[key_size + 1];
+  buffer[key_size] = '\0';
+  char filename[1024];
+  char alpha[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  uint32_t num_items;
+  uint32_t num_items_big = (uint32_t)((double)num_buckets_ * load_factor_max_);
+  uint32_t num_items_small = (uint32_t)((double)num_buckets_ * load_factor_remove_);
+  fprintf(stdout, "num_items %u %u\n", num_items_big, num_items_small);
+
+  std::string testcase = "ripple";
+  std::string directory = "results";
+  if (exists_or_mkdir(directory.c_str()) != 0) {
+    fprintf(stderr, "Could not create directory [%s]\n", testcase.c_str());
+    exit(1);
+  }
+
+  char pt_string[1024];
+  sprintf(pt_string, "lfm%.2f-lfr%.2f", load_factor_max_, load_factor_remove_);
+
+  char pt_json[1024];
+  sprintf(pt_json, "{\"load_factor_max\": %.2f, \"load_factor_remove\": %.2f}", load_factor_max_, load_factor_remove_);
+
+  std::set<std::string>::iterator it_find;
+  for (int i = 0; i < 10; i++) {
+    num_items = num_items_big;
+    srand(i);
+    keys.clear();
+    hm_->Open();
+    for (int cycle = 0; cycle < 50; cycle++) {
+      fprintf(stderr, "instance %d cycle %d\n", i, cycle);
+      for (uint32_t j = 0; j < num_items; j++) {
+        bool is_valid = false;
+        while (!is_valid) {
+          for (int k = 0; k < key_size; k++) {
+            buffer[k] = alpha[rand() % 62];
+          }
+          key = buffer;
+          it_find = keys.find(key);
+          if (it_find == keys.end()) {
+            is_valid = true;
+          } else {
+            //fprintf(stdout, "%s\n", key.c_str());
+            //fprintf(stdout, "%d\n", keys.size());
+          }
+        }
+        keys.insert(key);
+        int ret_put = hm_->Put(key, key);
+        //fprintf(stderr, "Put() [%s]\n", key.c_str());
+        if (ret_put != 0) {
+          fprintf(stderr, "Put() error\n");
+        }
+
+        if (cycle > 0) {
+          uint64_t r = rand();
+          uint64_t offset = r % keys.size();
+          //printf("delete index %d -- offset %llu -- rand %llu\n", index_del, offset, r);
+          std::set<std::string>::iterator it(keys.begin());
+          std::advance(it, offset);
+          //fprintf(stdout, "str: %s\n", (*it).c_str());
+          //key = buffer;
+          int ret_remove = hm_->Remove(*it);
+          //fprintf(stderr, "Remove() [%s]\n", it->c_str());
+          if (ret_remove != 0) fprintf(stderr, "Error while removing\n");
+          keys.erase(it);
+        }
+      }
+      printf("keys insert %zu\n", keys.size());
+
+      hm_->monitoring_->SetTestcase(testcase);
+      hm_->monitoring_->SetInstance(i);
+      hm_->monitoring_->SetCycle(cycle);
+      hm_->monitoring_->SetParametersTestcaseString(pt_string);
+      hm_->monitoring_->SetParametersTestcaseJson(pt_json);
+
+      std::map<std::string, std::string> metadata;
+      hm_->GetMetadata(metadata);
+      sprintf(filename, "%s/%s-%s-%s--%s-density--instance%05d-cycle%04d.json", directory.c_str(), testcase.c_str(), metadata["name"].c_str(), metadata["parameters_hashmap_string"].c_str(), pt_string, i, cycle);
+      hm_->monitoring_->PrintDensity(filename);
+
+      fprintf(stderr, "PrintDensity() out\n");
+      //sprintf(filename, "%s/%s-%s-num_scanned_blocks-%05d-%04d.json", testcase.c_str(), testcase.c_str(), metadata["name"].c_str(), i, cycle);
+      //hm->monitoring_->PrintNumScannedBlocks(filename);
+
+      sprintf(filename, "%s/%s-%s-%s--%s-psl--instance%05d-cycle%04d.json", directory.c_str(), testcase.c_str(), metadata["name"].c_str(), metadata["parameters_hashmap_string"].c_str(), pt_string, i, cycle);
+      fprintf(stderr, "filename psl %s\n", filename);
+      hm_->monitoring_->PrintProbingSequenceLengthSearch(filename);
+
+      num_items = num_items_small;
+    }
+
+    fprintf(stderr, "close\n");
+    hm_->Close();
+    fprintf(stderr, "ok\n");
+  }
+
+
+
+  // testcase-algo-metric-runnumber-step.json
+  // batch50-shadow-density-00001-0001.json
+  //hm_->monitoring_->PrintDensity("density.json");
+  //hm_->monitoring_->PrintNumScannedBlocks("num_scanned_blocks.json");
+}
+
+
+
+
+
+};
---
+++ testcase.h
@@ -0,0 +1,80 @@
+#ifndef HASHMAP_TESTCASE
+#define HASHMAP_TESTCASE
+
+#ifndef __STDC_FORMAT_MACROS
+#define __STDC_FORMAT_MACROS
+#endif
+#include <inttypes.h>
+#include <string.h>
+#include <stdio.h>
+#include <errno.h>
+
+#include <string>
+//#include <iostream>
+//#include <map>
+#include <set>
+#include <algorithm>
+#include <sys/stat.h>
+
+
+#include "hashmap.h"
+#include "monitoring.h"
+
+
+namespace hashmap
+{
+
+class TestCase {
+ public:
+  TestCase() {}
+  virtual ~TestCase() {}
+  virtual void run() = 0;
+  void InsertEntries(uint32_t nb_items, std::set<std::string>& keys);
+  void RemoveEntries(uint32_t nb_items, std::set<std::string>& keys);
+  HashMap *hm_;
+};
+
+
+class BatchTestCase: public TestCase {
+
+ public:
+  BatchTestCase(HashMap *hm, uint64_t num_buckets, double load_factor_max, double load_factor_remove) {
+    hm_ = hm;
+    num_buckets_ = num_buckets;
+    load_factor_max_ = load_factor_max;
+    load_factor_remove_ = load_factor_remove;
+  }
+  virtual void run();
+
+ private:
+  uint64_t num_buckets_;
+  double load_factor_max_;
+  double load_factor_remove_;
+};
+
+
+
+class RippleTestCase: public TestCase {
+
+ public:
+  RippleTestCase(HashMap *hm, uint64_t num_buckets, double load_factor_max, double load_factor_remove) {
+    hm_ = hm;
+    num_buckets_ = num_buckets;
+    load_factor_max_ = load_factor_max;
+    load_factor_remove_ = load_factor_remove;
+  }
+  virtual void run();
+
+ private:
+  uint64_t num_buckets_;
+  double load_factor_max_;
+  double load_factor_remove_;
+};
+
+
+
+
+
+};
+
+#endif
